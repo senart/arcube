6ba1f7a3d942dd8c3e2d45ed794c015fe0815b6f
senart <gavril.tonev@gmail.com>
2015-12-20 19:23:31 +0200

    I messed up the things big time here

---
+++ Assets/VuforiaCameraMode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class VuforiaCameraMode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		VuforiaBehaviour.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		VuforiaBehaviour.Instance.RegisterOnPauseCallback(OnPaused);
+	}
+
+	private void OnVuforiaStarted()
+	{
+		bool focusModeSet = CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		if (!focusModeSet) {
+			Debug.Log("Camera Focus mode NOT set");
+		}
+	}
+	
+	private void OnPaused(bool paused)
+	{
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		}
+	}
+}
